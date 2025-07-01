ae7a462ce897da53698adf908428d55e18895bd6
KubaAdamczykKolejnyGit <adamczyk@kubaadamczyk.pl>
2025-07-01 22:34:48 +0200

    yeah its working

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    pl: 'Polski',
+    en: 'English'  
+};
+
+export const defaultLang = 'pl';
+
+export const ui = {
+    pl: {
+        'nav.home': 'Start',
+        'nav.about': 'O nas',    
+        'nav.blog': 'Blog', 
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About us',  
+        'nav.blog': 'Blog',   
+    },
+  
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,30 @@
+// const definedLanguages = ["en"]
+
+// export function getLangFromUrl(url: URL) {
+//   const [, lang] = url.pathname.split('/');
+//   return lang;
+// }
+
+import { ui, defaultLang } from './ui';
+
+export function getLinkWithLang(lang: keyof typeof ui, path: string) {  
+  return (lang === defaultLang ? "": `/${lang}`) + "/" + path;
+}
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
+
+export function getSameUrlInNewLang(url: URL, newLang: keyof typeof ui) {
+    const prevLang = getLangFromUrl(url);
+    const urlWithoutLang = (`${url.pathname}`).substring(prevLang === defaultLang ? 1 : (prevLang.length + 2));
+    return  getLinkWithLang(newLang, urlWithoutLang);
+}
