1ad905d91b6b3d694efd8c0d3ec4ee8f78f1c440
ElephantC-PL <elephantc@kubaadamczyk.pl>
2025-07-26 02:07:36 +0200

    rss in multiple languages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,6 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const bloga = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/bloga', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
-});
-
 const blog = defineCollection({	
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),	
 	schema: ({ image }) => z.object({
@@ -53,4 +39,4 @@ const nested = defineCollection({
 	}),
 });
 
-export const collections = { bloga, blog, home, aboutUs, nested };
+export const collections = { blog, home, aboutUs, nested };
